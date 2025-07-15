01f9e41880b61bbe42d6b76a00c7b43b3e64c2ec
MMuArFF <muarf@protonmail.com>
2025-07-15 14:58:31 +0200

    Supprime les fichiers ignorés du dépôt

--- firefox_extension/popup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('sendUrl').addEventListener('click', function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var currentTab = tabs[0];
-    var currentUrl = currentTab.url;
-
-    chrome.runtime.sendMessage({action: 'sendUrl', url: currentUrl});
-  });
-});
